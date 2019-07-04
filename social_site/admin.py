56ac7cff0811ebe625e5c21cabc2fe0fdc7b4ff2
majuba <mjbackhaus@gmail.com>
2019-07-04 23:46:04 +0200

    added hobby to admin

--- social_site/admin.py
+++ social_site/admin.py
@@ -1,13 +1,17 @@
 from django.contrib import admin
 
-from .models import Nutzer
+from .models import Nutzer, Hobby
 from django.contrib import admin
 
 class MyAdminSite(admin.AdminSite):
     site_header='Minifacebook Admin'
 admin_site = MyAdminSite(name='myadmin')
 
-#admin_site.register(Nutzer)
+
+@admin.register(Hobby, site=admin_site)
+class HobbyAdmin(admin.ModelAdmin):
+    list_display = ('h_name',)
+
 @admin.register(Nutzer, site=admin_site)
 class NutzerAdmin(admin.ModelAdmin):
     #fields = ('n_name', 'anzeigename', 'id')
